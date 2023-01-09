ae88b584d3954dbe2aa6b6f71d77a654ee4bedae
Vladislav <typlomi@gmail.com>
2023-01-09 16:20:44 +0600

    create stert function

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+const start = async () => {
+  try {
+    const PORT = process.env.PORT || 9000;
+    const app = await NestFactory.create(AppModule);
+    await app.listen(PORT, () => {
+      console.log(`server started on port ${PORT}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
