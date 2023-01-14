c060e66523fbaea955af9c061496523c6b38fbce
Vladislav <typlomi@gmail.com>
2023-01-14 20:55:49 +0600

    created album.schema

--- client/store/TrackStore.ts
+++ client/store/TrackStore.ts
@@ -1,4 +1,4 @@
-import { makeAutoObservable } from "mobx";
+import { makeAutoObservable, runInAction } from "mobx";
 import { ITrack } from "../types/track";
 import axios from '../axios';
 
@@ -13,20 +13,28 @@ class TrackStore {
   fetchTracks = async () => {
     try {
       const response = await axios.get<ITrack[]>('/tracks');
-      this.currentTracks = response.data;
+      runInAction(() => {
+        this.currentTracks = response.data;
+      })
     } catch (err) {
-      console.warn(err);
-      return 'Не удалось загрузить треки'
+      runInAction(() => {
+        console.warn(err);
+        return 'Не удалось загрузить треки'
+      })
     }
   }
 
   searchTracks = async (query: string) => {
     try {
       const response = await axios.get<ITrack[]>('/tracks/search?query=' + query);
-      this.currentTracks = response.data;
+      runInAction(() => {
+        this.currentTracks = response.data;
+      })
     } catch (err) {
-      console.warn(err);
-      return 'Не удалось загрузить треки'
+      runInAction(() => {
+        console.warn(err);
+        return 'Не удалось загрузить треки'
+      })
     }
   }
 }
