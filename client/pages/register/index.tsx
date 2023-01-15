3ab77f75cb919803c2e7aafce77f4a8b1115b5c5
Vladislav <typlomi@gmail.com>
2023-01-15 21:55:58 +0600

    Форма регистрации, отображение аватарки при загрузке

--- client/pages/register/index.tsx
+++ client/pages/register/index.tsx
@@ -1,15 +1,69 @@
-import { Box, ThemeProvider } from "@mui/material";
+import { Avatar, Box, Button, Grid, TextField, ThemeProvider, Typography } from "@mui/material";
 import React from "react";
 import MainLayout from "../../layouts/MainLayout";
 import theme from "../../theme/theme";
 
 const Index: React.FC = () => {
+  const fileInput = React.useRef<HTMLInputElement | null>(null);
+  const [avatar, setAvatar] = React.useState('');
+  const changeAvatar = (evt: React.ChangeEvent<HTMLInputElement>) => {
+    if(evt.target.files) {
+      const file = evt.target.files[0];
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        setAvatar(e?.target?.result as string)
+      }
+      reader.readAsDataURL(file);
+    }
+  }
   return (
     <ThemeProvider theme={theme}>
       <MainLayout>
-        <Box>
-          Register
-        </Box>
+        <Grid 
+        container 
+        direction='column' 
+        width={400} 
+        margin='0 auto' 
+        gap={5} 
+        alignItems='center' 
+        >
+          <Typography variant="h4" fontWeight={800}>Регистрация</Typography>
+          <Avatar 
+          src={avatar}
+          sx={{width: 120, height: 120, cursor: 'pointer', bgcolor: '#5824f3'}}
+          onClick={() => fileInput.current?.click()}
+          />
+          <input 
+          ref={fileInput}
+          accept='image/*'
+          type="file"
+          onChangeCapture={changeAvatar}
+          hidden
+          />
+          <TextField
+          fullWidth
+          label='Email'
+          required
+          type="email"
+          />
+          <TextField
+          fullWidth
+          label="Имя/Никнейм"
+          required
+          type="text"
+          />
+          <TextField
+          fullWidth
+          label="Пароль"
+          required
+          type="password"
+          />
+          <Button
+          variant="contained"
+          >
+            Создать аккаунт
+          </Button>
+        </Grid>
       </MainLayout>
     </ThemeProvider>
   )
