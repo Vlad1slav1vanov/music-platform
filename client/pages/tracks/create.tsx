d5886ca3e529677d6d2ebed0bca4bd0710271bf4
Vladislav <typlomi@gmail.com>
2023-01-18 18:59:22 +0600

    Заверстал форму загрузки трека

--- client/pages/tracks/create.tsx
+++ client/pages/tracks/create.tsx
@@ -1,84 +1,83 @@
-import { Button, Grid, ThemeProvider } from '@mui/material';
+import { Box, Button, Grid, TextField, ThemeProvider, Typography } from '@mui/material';
 import React from 'react';
-import FirstStep from '../../components/FirstStep';
-import StepWrapper from '../../components/StepWrapper';
 import MainLayout from '../../layouts/MainLayout';
-import UploadStep from '../../components/UploadStep';
 import { useInput } from '../../hooks/useInput';
 import axios from '../../axios';
 import { useRouter } from 'next/router';
 import theme from '../../theme/theme';
+import styled from 'styled-components';
+
+const StyledForm = styled(Box)`
+  display: flex;
+  width: 100%;
+  margin: 0 auto;
+  gap: 30px;
+  align-items: flex-start;
+  justify-content: space-between;
+`
+
+const Img = styled.img`
+  display: block;
+  margin: 0 auto;
+  margin-bottom: 15px;
+`
 
 const Create = () => {
   const router = useRouter();
-  const [activeStep, setActiveStep] = React.useState(0);
-  const [picture, setPicture] = React.useState(null);
-  const [audio, setAudio] = React.useState(null);
-  const name = useInput('')
-  const artist = useInput('')
-  const text = useInput('')
 
-  const next = () => {
-    if (activeStep !== 2) {
-      setActiveStep(prev => prev + 1)
-    } else {
-      const formData = new FormData();
-      formData.append('name', name.value)
-      formData.append('artist', artist.value)
-      formData.append('text', text.value)
-      picture &&
-      formData.append('picture', picture)
-      audio &&
-      formData.append('audio', audio)
-      axios.post('/tracks', formData)
-        .then(res => router.push('/tracks'))
-        .catch(err => console.error(err))
-    }
-  }
-
-  const back = () => {
-    setActiveStep(prev => prev - 1)
-  }
+  // const next = () => {
+  //   if (activeStep !== 2) {
+  //     setActiveStep(prev => prev + 1)
+  //   } else {
+  //     const formData = new FormData();
+  //     formData.append('name', name.value)
+  //     formData.append('artist', artist.value)
+  //     formData.append('text', text.value)
+  //     picture &&
+  //     formData.append('picture', picture)
+  //     audio &&
+  //     formData.append('audio', audio)
+  //     axios.post('/tracks', formData)
+  //       .then(res => router.push('/tracks'))
+  //       .catch(err => console.error(err))
+  //   }
+  // }
 
   return (
     <ThemeProvider theme={theme}>
       <MainLayout>
-        <StepWrapper activeStep={activeStep}>
-          {activeStep === 0 &&
-            <FirstStep 
-            name={name} 
-            artist={artist} 
-            text={text} 
-            />   
-          }
-          {activeStep === 1 &&
-            <UploadStep 
-            title='Загрузите обложку' 
-            accept='image/*' 
-            setFile={setPicture} 
-            />
-          }
-          {activeStep === 2 &&
-            <UploadStep 
-            title='Загрузите аудиофайл' 
-            accept='audio/*' 
-            setFile={setAudio} 
-            />
-          }
-        </StepWrapper>
-        <Grid container justifyContent='space-between'>
-          <Button 
-          onClick={back}
-          disabled={activeStep === 0}
-          >
-            Назад
+        <Typography variant='h4' marginBottom={3}>Загрузить трек</Typography>
+        <StyledForm>
+          <Img width={200} height={200} />
+          <Grid container direction="column" sx={{gap: '15px'}} >
+            <TextField label="Название" required />
+            <TextField label="Исполнитель" required />
+            <TextField label="Текст" multiline />
+          </Grid>
+        </StyledForm>
+        <Box sx={{margin: '15px 0', display: 'flex', gap: '30px'}} >
+          <input type="file" hidden accept='image/*' />
+          <Button variant='contained'>
+            загрузить обложку
           </Button>
-          <Button 
-          onClick={next}
-          >
-            Далее
-          </Button>
-        </Grid>
+          <Button variant='outlined' color='error'>Удалить</Button>       
+        </Box>
+        <Box sx={{marginBottom: '15px', display: 'flex', gap: '54px'}}>
+          <input type="file" hidden accept='audio/*' />
+          <Button variant="contained" >загрузить аудио</Button>
+          <Button variant='outlined' color='error' hidden >Удалить</Button>
+        </Box>
+        <Box 
+        sx={{
+        width: '100%', 
+        display: 'flex', 
+        justifyContent: 'center', 
+        gap: '20px', 
+        marginTop: '15px'
+        }}>
+          <Button variant='contained'>Загрузить</Button>
+          <Button variant='outlined'>Сброс</Button>
+        </Box>
       </MainLayout>
     </ThemeProvider>
   )
